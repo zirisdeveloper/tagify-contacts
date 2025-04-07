ded9f3398569d7066ac31de55d132902cc759bb1
zirijob <zirijob@gmail.com>
2025-04-07 10:53:00 +0100

    MB-changes

---
+++ android/app/src/main/java/com/zirisdeveloper/piston/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zirisdeveloper.piston;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zirisdeveloper.piston',
+  appName: 'Piston',
+  webDir: 'dist'
+};
+
+export default config;
